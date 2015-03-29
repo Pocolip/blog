13a4d7e2e83e2ecc49e0799aa18a7a971b56dee1
Luis Medina <luis.medina14@upr.edu>
2015-03-29 01:23:19 -0400

    Added new post page, separated css, added jquery, plus a couple of new dependencies

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ var views = require("koa-views");
 var Router = require("koa-router");
 var serve = require("koa-static-folder");
 var handlebars = require("koa-handlebars");
+var serve = require('koa-static-folder');
+var marked = require('marked');
+var parse = require('co-body');
+var morph = require('morph');
 var route = new Router();
 
 //External Data
 var db = require("./db");
+
 
 //Start the app
 var app = koa();
@@ -25,6 +30,9 @@ app.use(handlebars({
         debug: function(obj){
             console.log("Working.");
             console.log(obj);
+        },
+        add: function(obj){
+            return obj;
         }
 
     }
@@ -32,9 +40,14 @@ app.use(handlebars({
 }));
 
 app.use(logger());
+app.use(serve("./js"));
+app.use(serve("./css"));
+
 
 route.get("/", index);
-
+route.get("/post/:link", post);
+route.get("/new", add);
+route.post("/create", create);
 app.use(route.routes());
 
 
@@ -42,6 +55,33 @@ function *index(){
     yield this.render("index", {
         posts : db.posts
     });
+}
+
+function *post(){
+    var new_post;
+    for(var i = 0; i < db.posts.length; i++) {
+        if (this.params.link == db.posts[i].link) {
+            new_post = db.posts[i];
+        }
+    }
+    yield this.render("post",{
+        title: new_post.title,
+        date: new_post.date,
+        text: marked(new_post.text)
+    });
+}
+function *add(){
+    yield this.render("new");
+}
+
+function *create(){
+    console.log("hi");
+
+    var post = yield parse(this);
+    db.posts.push(post);
+    post.date = new Date;
+    post.link = morph.toSnake(post.title);
+    this.redirect('/');
 }
 
 //Set the port
