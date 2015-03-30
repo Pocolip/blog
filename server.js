debb7256706115271b760a402eb70d60d04f1ffd
Luis Medina <luis.medina14@upr.edu>
2015-03-30 01:41:32 -0400

    Change footer again, needs more tweaking, started working with mongo

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ var views = require("koa-views");
 var Router = require("koa-router");
 var serve = require("koa-static-folder");
 var handlebars = require("koa-handlebars");
-var serve = require('koa-static-folder');
 var marked = require('marked');
 var parse = require('co-body');
 var morph = require('morph');
 var route = new Router();
 
-//External Data
-var db = require("./db");
+//Environment file
+var env = require('node-env-file'); //process.env.whatever
+env(__dirname + '/.env');
 
+//Database
+var mongojs = require('mongojs');
+var db = mongojs(process.env.HOST + '/' + process.env.NAME, ['posts']);
+
+//Check connection
+db.runCommand({ping:1}, function(err, res) {
+    if(!err && res.ok) console.log("Connected to DB");
+});
 
 //Start the app
 var app = koa();
@@ -50,20 +58,22 @@ route.get("/new", add);
 route.post("/create", create);
 app.use(route.routes());
 
-
 function *index(){
+    var list;
+    db.posts.find({}, function(err, docs){
+        return docs;
+    });
+
     yield this.render("index", {
-        posts : db.posts
+        posts : list
     });
 }
 
 function *post(){
-    var new_post;
-    for(var i = 0; i < db.posts.length; i++) {
-        if (this.params.link == db.posts[i].link) {
-            new_post = db.posts[i];
-        }
-    }
+    var new_post = db.posts.findOne({link: this.params.link}, function(err, doc){
+        return doc;
+    });
+
     yield this.render("post",{
         title: new_post.title,
         date: new_post.date,
@@ -75,12 +85,10 @@ function *add(){
 }
 
 function *create(){
-    console.log("hi");
-
     var post = yield parse(this);
-    db.posts.push(post);
     post.date = new Date;
     post.link = morph.toSnake(post.title);
+    db.posts.insert(post);
     this.redirect('/');
 }
 
